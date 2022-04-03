24ae409d261d8480709827a2af2fabf7837a1288
xKashalot <101393967+xKashalot@users.noreply.github.com>
2022-04-03 23:54:50 +0300

    Add files via upload

---
+++ PP_2_1_2_Spring_HelloWorld-master/PP_2_1_2_Spring_HelloWorld-master/src/main/java/App.java
@@ -0,0 +1,28 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getName());
+
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getName());
+
+        System.out.println(bean == bean2);
+
+        System.out.println(cat == cat2);
+
+
+
+    }
+}
