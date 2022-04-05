74087993bc6ec2e5c46f67a67b1e6ade42115ac7
xKashalot <101393967+xKashalot@users.noreply.github.com>
2022-04-05 14:39:24 +0300

    Add files via upload

---
+++ PP_2_1_4_spring-types-of-wiring-master/PP_2_1_4_spring-types-of-wiring-master/src/main/java/koschei/models/Ocean1.java
@@ -0,0 +1,18 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Ocean1 {
+
+    @Autowired
+    @Qualifier("island2")
+    private Island2 island;
+
+    @Override
+    public String toString() {
+        return "на океане остров " + island.toString();
+    }
+}
---
+++ PP_2_1_4_spring-types-of-wiring-master/PP_2_1_4_spring-types-of-wiring-master/src/main/java/koschei/models/Rabbit4.java
@@ -0,0 +1,21 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Rabbit4 {
+
+    @Autowired
+    private final Duck5 duck;
+
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
+    }
+
+    @Override
+    public String toString() {
+        return ", в зайце утка " + duck.toString();
+    }
+}
