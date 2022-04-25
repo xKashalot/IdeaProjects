60f6b568f8279e0065afb83744fee51e5fd96d26
xKashalot <101393967+xKashalot@users.noreply.github.com>
2022-04-25 14:39:58 +0300

    Update CarService.java

--- PP_2_2_2_spring-mvc-master/PP_2_2_2_spring-mvc-master/src/main/java/web/service/CarService.java
+++ PP_2_2_2_spring-mvc-master/PP_2_2_2_spring-mvc-master/src/main/java/web/service/CarService.java
@@ -11,21 +11,4 @@ import java.util.stream.Collectors;
 
 public interface CarService {
     List<Car> showCars(int count);
-
-//     static List<Car> carList() {
-//        List<Car> carList = new ArrayList<>();
-//        carList.add(new Car("BMW", 2010, 200));
-//        carList.add(new Car("Mazda", 2015, 150));
-//        carList.add(new Car("Audi", 2000, 100));
-//        carList.add(new Car("Volkswagen", 2020, 400));
-//        carList.add(new Car("Toyota", 1990, 250));
-//        return carList;
-//    }
-//
-//    static List<Car> cars(List<Car> carList, int count){
-//        if (count == 0 || count > 5) {
-//            return carList;
-//        }
-//        return carList.stream().limit(count).collect(Collectors.toList());
-//    }
 }
