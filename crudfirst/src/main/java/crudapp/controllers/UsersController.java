ce04afdbcd6613901f6456166a6cd47509274872
xKashalot <101393967+xKashalot@users.noreply.github.com>
2022-05-05 18:00:02 +0300

    Add files via upload

--- crudfirst/src/main/java/crudapp/controllers/UsersController.java
+++ crudfirst/src/main/java/crudapp/controllers/UsersController.java
@@ -11,10 +11,8 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/users")
 public class UsersController {
-
     @Autowired
     private final UserServiceImpl userService;
-
 
     public UsersController(UserServiceImpl userService) {
         this.userService = userService;
@@ -24,12 +22,6 @@ public class UsersController {
     public String index(Model model) {
         model.addAttribute("users", userService.users());
         return "users/index";
-    }
-
-    @GetMapping("/{id}")
-    public String showById(@PathVariable("id") int id, Model model) {
-        model.addAttribute("user", userService.showUser(id));
-        return "users/user";
     }
 
     @GetMapping("/new")
@@ -43,21 +35,58 @@ public class UsersController {
         return "redirect:/users";
     }
 
-    @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id){
+    @PostMapping("/view")
+    public String showById(@RequestParam("id") int id, Model model) {
         model.addAttribute("user", userService.showUser(id));
-        return "users/edit";
+        return "users/user";
     }
 
-    @PatchMapping("/{id}")
-    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+    @PostMapping("/update")
+    public String update(@ModelAttribute("user") User user, @RequestParam("id") int id) {
         userService.update(id, user);
         return "redirect:/users";
     }
 
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
+    @PostMapping("/")
+    public String delete(@RequestParam("id") int id) {
         userService.delete(id);
         return "redirect:/users";
     }
+
+    @PostMapping("/edit")
+    public String edit(@RequestParam("id") int id, Model model){
+    model.addAttribute("user", userService.showUser(id));
+    return "users/edit";
+    }
+
+
+
+
+
+
+//        @GetMapping("/{id}")
+//    public String showById(@PathVariable("id") int id, Model model) {
+//        model.addAttribute("user", userService.showUser(id));
+//        return "users/user";
+//    } rest
+
+//        @GetMapping("/{id}/edit")
+//    public String edit(Model model, @PathVariable("id") int id){
+//        model.addAttribute("user", userService.showUser(id));
+//        return "users/edit";
+//    } rest
+
+
+//        @PatchMapping("/{id}")
+//    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+//        userService.update(id, user);
+//        return "redirect:/users";
+//    } rest
+
+//    @DeleteMapping("/{id}")
+//    public String delete(@PathVariable("id") int id) {
+//        userService.delete(id);
+//        return "redirect:/users";
+//    } rest
+
 }
